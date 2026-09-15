8c7371cca7610075894ae8e36df0754f20c78b32
Joyita Sadhukhan <80900114+joyee99@users.noreply.github.com>
2026-09-15 11:23:18 +0530

    Update 3_reorder_behavior.sql
    Products with Highest re-order rate query pushed.

--- 3_Analysis/3_reorder_behavior.sql
+++ 3_Analysis/3_reorder_behavior.sql
@@ -11,3 +11,23 @@ select *,
 round(cast(total_reordered as float)*100.0/total_items,2) as reordered_pct,
 round(cast(total_first_purchase as float)*100.0/total_items,2) as first_purchase_pct
 from reorder;
+
+
+
+-- 2. Products with highest reorder rate
+with reorder_pct as(
+	select product_id, 
+	round(cast(sum(reordered) as float)*100.0/count(*),2) as reorder_pct
+	from order_products
+	group by product_id having count(*)>=500
+),
+pct_rank as(
+	select rp.product_id, p.product_name, reorder_pct,
+	dense_rank() over(order by reorder_pct desc) as ranks
+	from reorder_pct rp join products p 
+	on rp.product_id=p.product_id
+)
+select product_id,product_name,reorder_pct from pct_rank where ranks<=20;
+
+
+
