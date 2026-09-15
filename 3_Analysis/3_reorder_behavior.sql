66744928ce2d812c8c56171b2f21ba89f676c15f
Joyita Sadhukhan <80900114+joyee99@users.noreply.github.com>
2026-09-15 10:27:34 +0530

    Update 3_reorder_behavior.sql
    Overall reorder rate sql query pushed.

--- 3_Analysis/3_reorder_behavior.sql
+++ 3_Analysis/3_reorder_behavior.sql
@@ -1,1 +1,13 @@
+-- 1. Overall reorder rates.
 
+with reorder as(
+	select
+		count(order_id) as total_items,
+		sum(reordered) as total_reordered,
+		sum(case when reordered=0 then 1 end) as total_first_purchase
+	from order_products
+)
+select *, 
+round(cast(total_reordered as float)*100.0/total_items,2) as reordered_pct,
+round(cast(total_first_purchase as float)*100.0/total_items,2) as first_purchase_pct
+from reorder;
