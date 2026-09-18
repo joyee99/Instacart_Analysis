91a1158a88b1523bbbd232c65cf9e822fb79d096
Joyita Sadhukhan <80900114+joyee99@users.noreply.github.com>
2026-09-18 11:45:31 +0530

    Update 5_Time_and_Trend_Analysis.sql
    Busiest day of week is found through query 2.

--- 3_Analysis/5_Time_and_Trend_Analysis.sql
+++ 3_Analysis/5_Time_and_Trend_Analysis.sql
@@ -29,3 +29,21 @@ group by o.all_orders;
 
 
 
+-- 2. Busiest Day of the week
+with overall as(
+	select count(distinct order_id) as overall_orders from orders
+)
+select order_dow,
+case when order_dow=0 then 'Saturday'
+	 when order_dow=1 then 'Sunday'
+	 when order_dow=2 then 'Monday'
+	 when order_dow=3 then 'Tuesday'
+	 when order_dow=4 then 'Wednesday'
+	 when order_dow=5 then 'Thursday'
+	 when order_dow=6 then 'Friday'
+end as day_name,
+count(order_id) as total_orders,
+round(cast(count(order_id) as float)/(select overall_orders from overall)*100,2) as weekday_order_pct
+from orders 
+group by order_dow 
+order by 3 desc;
